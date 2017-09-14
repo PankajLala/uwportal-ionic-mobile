b516a182b4369e03748325c83e801582bc682c71
Pankaj <pankaj.lala@xceedance.com>
2017-09-14 16:33:10 +0530

    login page

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,9 +1,16 @@
 import {Component} from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {PropertyListPage} from '../property-list/property-list';
 
 @Component({
     selector: 'page-welcome',
     templateUrl: 'welcome.html'
 })
 export class WelcomePage {
+ constructor(public navCtrl: NavController) {
+  }
 
+login(){
+  this.navCtrl.setRoot(PropertyListPage);
+  }
 }
